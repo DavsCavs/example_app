a612914311c63b71e85ddfa78d235dcb89960f58
unknown <your@email.example>
2024-10-17 14:18:00 +0300

    Page switch

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>All posts</h1>
+    <ul>
+    @foreach($allPosts as $post)
+    <li>
+        Title: {{$post->title}}<br>
+        Content: {{$post->content}}<br>
+        <a href="/posts/{{ $post->id}}">Show </a>
+    </li>
+    @endforeach
+    </ul>
+</body>
+</html>
